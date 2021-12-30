9b953ea591e09ff0589549ce04d2fd843123253e
THEEKSHANA <87897206+ImTheekshana126@users.noreply.github.com>
2021-12-30 21:48:28 +0530

    radio

--- radio/start.py
+++ radio/start.py
@@ -5,11 +5,11 @@ from config import BOT_USERNAME, BOT_NAME
 @Client.on_message(filters.command("start"))
 async def start(client, m: Message):
    if m.chat.type == 'private':
-       await m.reply(f"**Hey I am Radio Bot 📻\n\n** \n`Add this @{BOT_USERNAME} to your Group and Make it Admin \n2) Add`  `to your Group` \n3) **Commands** : \n`/radio` Ytlink Live \n`/stop`",   
+       await m.reply(f"**Hey I am Radio Bot 📻\n\n** \nAdd this @{BOT_USERNAME} to your Group and Make it Admin \n2) Add  to your Group \n3) **Commands** : \n/radio Ytlink Live \n/stop",   
                             reply_markup=InlineKeyboardMarkup(
                                 [[
                                      InlineKeyboardButton(
-                                            "Support", url="t.me/DeCodeSupport")
+                                            "Support", url="t.me/theekshana_Support")
                                     ]]
                             ))
    else:
